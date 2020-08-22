0ca4358dfa7b186ecef97cd370eef989ac75cd89
chattika-srisook <chattika.sr@rmuti.ac.th>
2020-08-22 13:48:25 +0700

    add binding

--- app/src/main/java/com/example/myfirstappagain/MainActivity.kt
+++ app/src/main/java/com/example/myfirstappagain/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myfirstappagain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.myfirstappagain.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
     }
 }
