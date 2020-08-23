f17120140be2c19cf2354de63c13a6b315901345
chattika-srisook <chattika.sr@rmuti.ac.th>
2020-08-23 13:24:52 +0700

    Add navigation

---
+++ app/src/main/java/com/example/myfirstappagain/ContactFragment.kt
@@ -0,0 +1,18 @@
+package com.example.myfirstappagain
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ContactFragment : Fragment() {override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+): View? {
+    return  inflater.inflate(R.layout.fragment_contact,container,false)
+    // return super.onCreateView(inflater, container, savedInstanceState)
+}
+
+}
--- app/src/main/java/com/example/myfirstappagain/MainActivity.kt
+++ app/src/main/java/com/example/myfirstappagain/MainActivity.kt
@@ -2,17 +2,63 @@ package com.example.myfirstappagain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.myfirstappagain.databinding.ActivityMainBinding
+import com.google.android.material.internal.NavigationMenuView
+import com.google.android.material.navigation.NavigationView
 
-private lateinit var binding: ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+
+    private  lateinit var  binding:ActivityMainBinding
+    private  lateinit var  drawer_layout:DrawerLayout
+    private lateinit var navigation_view: NavigationView
+    private  lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        drawer_layout = binding.drawerlayout
+        navigation_view = binding.navView
+        navigation_view.setNavigationItemSelectedListener(this)
+        toggle = ActionBarDrawerToggle(this, drawer_layout,R.string.open,R.string.close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout?.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }else{
+
+            super.onBackPressed()
+        }
+    }
 
 
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_aboutme ->{
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_contain,AboutMeFragment()).commit()
+            }
+            R.id.nav_contact ->{
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_contain,ContactFragment()).commit()
+            }
+            R.id.nav_list ->{
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_contain,AddListFragment()).commit()
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
