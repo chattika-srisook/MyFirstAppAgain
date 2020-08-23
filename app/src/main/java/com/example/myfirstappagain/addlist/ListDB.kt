a6565f779e2fc5d7160677a265a377eee9e26184
chattika-srisook <chattika.sr@rmuti.ac.th>
2020-08-23 15:07:28 +0700

    create ListDB & ListDao

--- app/src/main/java/com/example/myfirstappagain/addlist/ListDB.kt
+++ app/src/main/java/com/example/myfirstappagain/addlist/ListDB.kt
@@ -1,4 +1,15 @@
 package com.example.myfirstappagain.addlist
 
-class ListDB {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "listTable")
+data class ListDB (
+    @PrimaryKey(autoGenerate = true)
+    var id:Int,
+    @ColumnInfo(name = "Name" ,defaultValue = "unknow")
+    var name:String,
+    @ColumnInfo(name = "email")
+    var email:String
+)
---
+++ app/src/main/java/com/example/myfirstappagain/addlist/ListDao.kt
@@ -0,0 +1,16 @@
+package com.example.myfirstappagain.addlist
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+@Dao
+interface ListDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(name:String,email:String)
+    @Query("select * from listTable order by id desc")
+    fun getList():LiveData<List<ListDB>>
+}
